71f108b443677dcd81b13eebdd86fc55ba6085ec
Matthew Kim <kimmatthew2022@gmail.com>
2018-07-26 11:48:33 -0700

    first upload

---
+++ todo-list/script.js
@@ -0,0 +1,16 @@
+let addbtn = document.querySelector("button");
+let input = document.querySelector("input")
+
+addbtn.addEventListener('click', function() {
+    // create
+    let btn = document.createElement("button");
+    btn.innerText = input.value;
+    btn.classList.add('todo-item');
+    document.body.appendChild(btn)
+
+    btn.addEventListener('click', function() {
+        document.body.removeChild(btn);
+    })
+
+    input.value = "";
+});
